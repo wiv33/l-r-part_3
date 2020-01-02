94269c6489341c9f65f6b8c4fff5e6d5d879722b
PSK <wiv.psk@gmail.com>
2020-01-02 21:58:10 +0900

    sample test 수정

--- src/test/java/com/psawesome/basepackage/learningreactivefile/sample/SampleTest.java
+++ src/test/java/com/psawesome/basepackage/learningreactivefile/sample/SampleTest.java
@@ -5,6 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.GroupedFlux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Comparator;
 
 /**
@@ -19,10 +20,21 @@ public class SampleTest {
         Flux.just("alpah", "bravo", "charlie")
                 .map(String::toUpperCase)
                 .flatMap(s -> Flux.fromArray(s.split("")))
-        .groupBy(String::toString)
-        .sort(Comparator.comparing(GroupedFlux::key))
-        .flatMap(group -> Mono.just(group.key()).and(group.count()))
+                .groupBy(String::toString)
+                .sort(Comparator.comparing(GroupedFlux::key))
+                .flatMap(group -> Flux.zip(Mono.just(group.key()), group.count()))
                 .map(keyAndCount -> keyAndCount.toString())
                 .subscribe(System.out::println);
+    }
+
+    @Test
+    public void generateAndDuration() {
+        Flux<String> generate = Flux.generate(() -> 1L, (id, sink) -> {
+            sink.next("value " + id);
+            return id + 1;
+        });
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1)).take(10);
+
+        /*return */Flux.zip(generate, interval).subscribe();
     }
 }
