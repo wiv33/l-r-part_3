cda742e8509436e45ae6dab61094168daeef8173
PS K <wiv821253@gmail.com>
2020-01-08 00:00:06 +0900

    add EndToEndTests.java

--- src/test/java/com/psawesome/basepackage/learningreactivefile/EndToEndTests.java
+++ src/test/java/com/psawesome/basepackage/learningreactivefile/EndToEndTests.java
@@ -1,20 +1,27 @@
 package com.psawesome.basepackage.learningreactivefile;
 
 
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.interactions.Actions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.event.annotation.AfterTestClass;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.openqa.selenium.chrome.ChromeDriverService.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.chrome.ChromeDriverService.createDefaultService;
 
 /**
  * package: com.psawesome.basepackage.learningreactivefile
@@ -32,11 +39,13 @@ public class EndToEndTests {
     @LocalServerPort
     int port;
 
-    @BeforeTestClass
-    public static void setUp () throws IOException {
-        System.setProperty("webdriver.chrome.driver", "ext/chromedriver");
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        System.out.println("EndToEndTests.setUp");
+        System.setProperty("webdriver.chrome.driver", "ext/chromedriver2");
         service = createDefaultService();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(service);
 
         Path testResults = Paths.get("build", "test-results");
 
@@ -45,9 +54,39 @@ public class EndToEndTests {
         }
     }
 
-    @AfterTestClass
-    public static void tearDown() {
+    @AfterAll
+    static void afterAll() {
         service.stop();
     }
 
+
+    @Test
+    void homePageShouldWork() throws IOException {
+        driver.get("http://localhost:" + port);
+        //스크린 샷
+        takeScreenshot("homePageShouldWork-1");
+
+        //페이지 제목 확인
+        assertThat(driver.getTitle()).isEqualTo("Learning Reactive");
+
+        String pageContent = driver.getPageSource();
+
+        assertThat(pageContent).contains("<a href=\"/images/bazinga.png/raw\">");
+
+        WebElement element = driver.findElement(By.cssSelector("a[href*=\"bazinga.png\"]"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+
+        takeScreenshot("homePageShouldWork-2");
+
+        driver.navigate().back();
+    }
+
+    private void takeScreenshot(String name) throws IOException {
+        FileCopyUtils.copy(
+            driver.getScreenshotAs(OutputType.FILE),
+            new File("/build/test-results/TEST-" + name)
+        );
+    }
 }
